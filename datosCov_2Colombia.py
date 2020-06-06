11505aa15fbdca1400eaac86266f887bcaa37519
luiseveliogarciarestrepo <luiseveliogarciarestrepo@gmail.com>
2020-06-05 22:34:31 -0500

    +extraccion de listado de lugares-ciudad-departamento-pais de procedencia

--- datosCov_2Colombia.py
+++ datosCov_2Colombia.py
@@ -1,6 +1,6 @@
 import csv
 
-direccion_url= '/home/luise/Documents/programas/datosRecopilador/CovidColombia.csv' # Dirección web del archivo
+direccion_local= '/home/luise/Documents/programas/datosRecopilador/CovidColombia.csv' # Dirección local del archivo
 
 # Parámetros formato de impresión
 
@@ -9,8 +9,8 @@ m=10
 s=6
 j = '.'
 
-#  Quitar comentario para obtener la base de datos requerida
-##
+#  Quitar comentario para obtener la base de datos requerida y guardarla en disco local
+
 ##import requests
 ##direccion_url = 'https://www.datos.gov.co/api/views/gt2j-8ykr/rows.csv?accessType=DOWNLOAD&bom=true&format=true'
 ##datos_req = requests.get(direccion_url)
@@ -38,9 +38,8 @@ def ajusteFechas(row):  # Toma formato de fechas del archivo, hace la partición
         fm = row['Fecha de muerte'].partition('T')         # Fecha de muerte
         return fn[0], fd[0], fr[0], fm[0]
 
-def archivo(): # Importa el archivo y crea el objeto para hacerle el DictReader y crear el csv como diccionario (keys = nombre columna, Values= fila/columna)
-    #exampleFile = open('/home/luise/Documents/programas/datosRecopilador/CovidColombia.csv')
-    exampleFile = open(direccion_url)
+def archivo(): # Importa el archivo localy crea el objeto para hacerle el DictReader y crear el csv como diccionario (keys = nombre columna, Values= fila/columna)
+    exampleFile = open(direccion_local)
     exampleDictReader = csv.DictReader(exampleFile)
     return exampleDictReader
 
@@ -51,11 +50,47 @@ def formatoEimpresion():
           row['Estado'].ljust(l,j),  row['País de procedencia'].ljust(l,j),
           'Notificado: ', fechas[0].ljust(m,j), 'Diagnosticado: ',
           fechas[1].ljust(m,j),'Recuperado: ', fechas[2].ljust(m,j), 'Defunción: ', fechas[3].ljust(m,j))
-   
 
+
+
+   
+'''
 #  Programa principal
 
 exampleDictReader = archivo()
 for row in exampleDictReader:
     fechas = list(ajusteFechas(row))
     formatoEimpresion()
+'''
+
+
+
+# Ciudades
+ciudades = []
+for row in archivo():
+    if row['Ciudad de ubicación'] in ciudades:
+        pass
+    else:
+        ciudades.append(row['Ciudad de ubicación'])
+ciudades.sort()
+#print(ciudades)
+
+# Departamento
+departamento= []
+for row in archivo():
+    if row['Departamento o Distrito '] in departamento:
+        pass
+    else:
+        departamento.append(row['Departamento o Distrito '])
+departamento.sort()
+#print(departamento)
+
+# Paises de procedencia
+paises= []
+for row in archivo():
+    if row['País de procedencia'] in paises:
+        pass
+    else:
+        paises.append(row['País de procedencia'])
+paises.sort()
+#print(paises)
