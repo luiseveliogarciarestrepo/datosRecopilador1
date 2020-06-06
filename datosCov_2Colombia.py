fa8cecd36dcc59a4b862ea407d30bb0e9a12a864
luiseveliogarciarestrepo <luiseveliogarciarestrepo@gmail.com>
2020-06-05 22:06:42 -0500

    +funcion importar archivo y crea objeto cvs diccionario

--- datosCov_2Colombia.py
+++ datosCov_2Colombia.py
@@ -1,3 +1,7 @@
+import csv
+
+direccion_url= '/home/luise/Documents/programas/datosRecopilador/CovidColombia.csv' # Dirección web del archivo
+
 # Parámetros formato de impresión
 
 l=25
@@ -25,28 +29,24 @@ j = '.'
 ##
 ##playFile.close()
   
-#  Importar archivo csv a python
+# Funciones
 
-def ajusteDatos():
+def ajusteFechas(row):  # Toma formato de fechas del archivo, hace la partición en 'T' y devuelve el primer índice => 2020-06-05T00:00:00:00 => 2020-06-05
         fn = row['Fecha de notificación'].partition('T')   # Fecha de notificación
         fd = row['Fecha diagnostico'].partition('T')       # Fecha de diagnóstico
         fr = row['Fecha recuperado'].partition('T')        # Fecha recuperado
         fm = row['Fecha de muerte'].partition('T')         # Fecha de muerte
         return fn[0], fd[0], fr[0], fm[0]
-    
 
-import csv
-exampleFile = open('/home/luise/Documents/programas/datosRecopilador/CovidColombia.csv')
-exampleDictReader = csv.DictReader(exampleFile)
+def archivo(): # Importa el archivo y crea el objeto para hacerle el DictReader y crear el csv como diccionario (keys = nombre columna, Values= fila/columna)
+    #exampleFile = open('/home/luise/Documents/programas/datosRecopilador/CovidColombia.csv')
+    exampleFile = open(direccion_url)
+    exampleDictReader = csv.DictReader(exampleFile)
+    return exampleDictReader
 
+exampleDictReader = archivo()
 for row in exampleDictReader:
-    #fecha = ajusteDatos()
-    #fechas = list(fecha)
-    fechas = list(ajusteDatos())
-##    fn = row['Fecha de notificación'].partition('T')   # Fecha de notificación
-##    fd = row['Fecha diagnostico'].partition('T')       # Fecha de diagnóstico
-##    fr = row['Fecha recuperado'].partition('T')        # Fecha recuperado
-##    fm = row['Fecha de muerte'].partition('T')         # Fecha de muerte
+    fechas = list(ajusteFechas(row))
     print(str(exampleDictReader.line_num).ljust(s,j),row['Ciudad de ubicación'].
           ljust(l,j), row['atención'].ljust(l,j), row['Edad'].ljust(s,j),
           row['Sexo'].ljust(s,j), row['Tipo'].ljust(l,j),
