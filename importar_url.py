dbdfcbed7498461bd1f2f008f8765e9474b32e1a
luiseveliogarciarestrepo <luiseveliogarciarestrepo@gmail.com>
2020-07-11 18:03:27 -0500

    tasas mundiales

--- importar_url.py
+++ importar_url.py
@@ -9,7 +9,7 @@ direccion_url = 'https://www.datos.gov.co/api/views/gt2j-8ykr/rows.csv?accessTyp
 '''Uncomment para definir el Path'''
 
 direccion_local = '/home/luise/Documents/programas/datosRecopilador/CovidColombia.csv'     # En mi Compaq
-# direccion_local = '/Users/Dr.LuisEvelioRestrepoGarcia/datosRecopilador/CovidColombia.csv'    # En mi Apple
+#direccion_local = '/Users/Dr.LuisEvelioRestrepoGarcia/datosRecopilador/CovidColombia.csv'    # En mi Apple
 
 
 '''Obtiene la base de datos actualizada y la guardar en disco local.'''
